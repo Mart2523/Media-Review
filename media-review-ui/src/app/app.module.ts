7ca99cf61206e16a162acb8d2ab14a10e21a96bf
SamLP2000 <SamLP2000@outlook.com>
2022-07-27 19:55:00 -0500

    Added Angular UI

--- media-review-ui/src/app/app.module.ts
+++ media-review-ui/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BookComponent } from './book/book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    BookComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
