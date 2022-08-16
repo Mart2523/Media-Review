9e7ec69eafa20e7a386d0a1090cde9df2ab89ff5
johnn <johnny.halbert@gmail.com>
2022-08-15 19:43:52 -0500

    Added rest of files.

--- media-review-v2-api/src/main/java/org/example/controllers/BookController.java
+++ media-review-v2-api/src/main/java/org/example/controllers/BookController.java
@@ -1,10 +1,10 @@
-package org.example.models.Controllers;
+package org.example.controllers;
 
 
 
 import lombok.AllArgsConstructor;
 import org.example.models.Book;
-import org.example.models.Data.BookRepository;
+import org.example.models.data.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +27,8 @@ public class BookController {
     public List<Book> getBooks (){
         return bookRepository.findAll();
     }
-    @GetMapping("/getBook/{ISBN}")
+
+    @GetMapping("/getBook/{isbn}")
     public Optional<Book> getBook(@PathVariable String isbn){
         System.out.println(isbn);
         return bookRepository.findBookByIsbn(isbn);
