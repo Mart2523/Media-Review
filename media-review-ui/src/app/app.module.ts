4548b883aa3fc397a5211e306b97e18c32053555
SamLP2000 <SamLP2000@outlook.com>
2022-08-17 19:03:18 -0500

    Testing files

--- media-review-ui/src/app/app.module.ts
+++ media-review-ui/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatSliderModule,
+    MatInputModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
