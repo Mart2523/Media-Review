6520d24af4e46ac9c1db49562638031b22ae21e4
Kieran <kieranbarry51@gmail.com>
2022-08-22 18:13:55 -0500

    added book-display component

--- media-review-ui/src/app/app.module.ts
+++ media-review-ui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BookDisplayComponent } from './book-display/book-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BookDisplayComponent
   ],
   imports: [
     BrowserModule
