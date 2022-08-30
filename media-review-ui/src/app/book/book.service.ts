44ac367fd82386b5dd4098e81b63c1b0f7ba4820
cadem <cademartinez333@gmail.com>
2022-08-29 20:31:20 -0500

    Added Return Book Lists

--- media-review-ui/src/app/book/book.service.ts
+++ media-review-ui/src/app/book/book.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
-const baseUrl = 'https://localhost:8080/getBooks'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  private baseUrl = 'https://localhost:8080/getBooks'
+
   constructor(
     private http: HttpClient
   ) { }
 
   getBooks(){
-    return this.http.get(baseUrl);
+    return this.http.get(this.baseUrl);
   }
 
 }
