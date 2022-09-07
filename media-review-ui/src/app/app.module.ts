9dffc5d249087a715d813ef6ee4a31f5e15e4e7b
cadem <cademartinez333@gmail.com>
2022-09-07 16:31:00 -0500

    Merged from main

--- media-review-ui/src/app/app.module.ts
+++ media-review-ui/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
-
+import { AppRoutingModule} from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BookComponent } from './book/book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { BookComponent } from './book/book.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
